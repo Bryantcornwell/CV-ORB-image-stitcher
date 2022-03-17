e32db87e423b21c688b1bed06bbd819638bcc0e9
Bryant <brycornw@iu.edu>
2022-03-17 00:19:43 -0400

    Start outlining part1 program

---
+++ part1.py
@@ -0,0 +1,33 @@
+
+
+
+
+import cv2
+import numpy as np
+import sys
+
+# Step 1 Determine ORB Matching
+k = sys.argv[1]
+
+img1 = cv2.imread("part1-images/bigben_2.jpg", cv2.IMREAD_GRAYSCALE)
+img2 = cv2.imread("part1-images/bigben_3.jpg", cv2.IMREAD_GRAYSCALE)
+
+# you can increase nfeatures to adjust how many features to detect
+orb = cv2.ORB_create(nfeatures=1000)
+
+# detect features
+(keypoints1, descriptors1) = orb.detectAndCompute(img1, None)
+(keypoints2, descriptors2) = orb.detectAndCompute(img2, None)
+
+
+"""
+# put a little X on each feature
+for i in range(0, len(keypoints)):
+    print("Keypoint #%d: x=%d, y=%d, descriptor=%s, distance between this descriptor and descriptor #0 is %d" % (
+    i, keypoints[i].pt[0], keypoints[i].pt[1], np.array2string(descriptors[i]),
+    cv2.norm(descriptors[0], descriptors[i], cv2.NORM_HAMMING)))
+    for j in range(-5, 5):
+        img[int(keypoints[i].pt[1]) + j, int(keypoints[i].pt[0]) + j] = 0
+        img[int(keypoints[i].pt[1]) - j, int(keypoints[i].pt[0]) + j] = 255
+"""
+#cv2.imwrite("lincoln-orb.jpg", img1)
