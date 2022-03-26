f9644dac2c32515993799e26a483535f193d42e9
Bryant <brycornw@iu.edu>
2022-03-26 12:27:30 -0400

    Ready for testing orb-match function

--- part1.py
+++ part1.py
@@ -9,7 +9,7 @@ import numpy as np
 
 
 def euclidean_distance(point1, point2):
-    distance= np.sqrt(np.sum(np.power(point1 - point2, 2)))
+    distance = np.sqrt(np.sum(np.power(point1 - point2, 2)))
     return distance
 
 
@@ -24,13 +24,14 @@ def orb_sift_match(image_a, image_b):
     # E.g True/False
     # (True/False, distance, another_distance)
 
-    return '_3' in image_b or '_5' in image_b
+    #return '_3' in image_b or '_5' in image_b
 
-    img1 = cv2.imread("part1-images/bigben_2.jpg", cv2.IMREAD_GRAYSCALE)
-    img2 = cv2.imread("part1-images/bigben_3.jpg", cv2.IMREAD_GRAYSCALE)
+    # img1 = cv2.imread(image_a, cv2.IMREAD_GRAYSCALE)
+    img1 = cv2.imread(image_a)
+    img2 = cv2.imread(image_b)
 
     # you can increase nfeatures to adjust how many features to detect
-    orb = cv2.ORB_create(nfeatures=1000)
+    orb = cv2.ORB_create(nfeatures=200)
 
     # detect features
     (keypoints1, descriptors1) = orb.detectAndCompute(img1, None)
@@ -38,13 +39,13 @@ def orb_sift_match(image_a, image_b):
 
     # Test image
     test_img = cv2.hconcat([img1, img2])
+    draw = ImageDraw.Draw(test_img)
     #cv2.imshow('Horizontal', test_img)
     #cv2.waitKey(0)
 
     # Threshold
-
+    threshold = 0.05
     # Two for loops to iterate through each feature point and calculate the Euclidean Distance
-    """
     for p1 in keypoints1:
         previous_distance = 10000
         point_list = []
@@ -52,7 +53,7 @@ def orb_sift_match(image_a, image_b):
         nearest_match = 0
         second_match = 0
         for p2 in keypoints2:
-            point_list.append(p2)
+            #point_list.append(p2)
             point_distance = distance(p1, p2)
             distance_list.append(point_distance)
             if point_distance < previous_distance:
@@ -66,10 +67,15 @@ def orb_sift_match(image_a, image_b):
         distance_2ndclosest = p1 - second_match
         match = distance_closest / distance_2ndclosest
         # if match < threshold then it is a match
+        if match < threshold:
+            point_list.append([p1, nearest_match])
+            draw.line((p1, (nearest_match[0]+img1.width, nearest_match[1])), fill=(255, 0, 0), width=7)
         #   Create a visual indication between the matched points for both images
-        # else:pass
-    """
-    #cv2.imshow('Horizontal', test_img)
+        else:
+            pass
+
+    cv2.imshow('Horizontal', test_img)
+    cv2.waitKey(0)
     #cv2.imwrite("lincoln-orb.jpg", img1)
 
 def cluster_images(images):
@@ -85,7 +91,8 @@ def cluster_images(images):
 
 def main(images, output, k=2):
 
-    cluster_images(images)
+    orb_sift_match("part1-images/bigben_2.jpg", "part1-images/bigben_3.jpg")
+    #cluster_images(images)
 
 
 if __name__ == '__main__':
